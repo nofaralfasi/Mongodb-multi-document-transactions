3f6159b93f10ca0e57ad2d02b43129b6a54f2e63
nofar <nofar.alfasi@s.afeka.ac.il>
2019-03-01 12:46:55 +0200

    Add files via upload

---
+++ change-streams.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker run --rm -it --network host -v "$(pwd)"/target:/target openjdk:10.0-jre-slim java -cp /target/mongodb-nofar-project-1.0.0-jar-with-dependencies.jar com.mongodb.ChangeStreams mongodb://localhost/mongo
